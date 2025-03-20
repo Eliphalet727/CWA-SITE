0edd7fe4d42597c66df25e5291b3e938c719df14
Eliphalet727 <judas9010@gmail.com>
2025-03-20 15:06:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,22 @@ app.get('/od1', async (req, res) => {
   }
 });
 
+app.get('/od2', async (req, res) => {
+  const url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=rdec-key-123-45678-011121314&format=JSON';
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);  // 以 JSON 格式回給前端
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.get('/od3', async (req, res) => {
   const url = 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=rdec-key-123-45678-011121314&format=JSON';
 
